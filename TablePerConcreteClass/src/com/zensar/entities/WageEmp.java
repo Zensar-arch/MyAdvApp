1e40c3b113e9508e74dc4551f476f1c7748001c8
Zensar-arch <55914257+Zensar-arch@users.noreply.github.com>
2019-09-28 19:02:02 +0530

    Add files via upload

---
+++ TablePerConcreteClass/src/com/zensar/entities/WageEmp.java
@@ -0,0 +1,64 @@
+package com.zensar.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Susmita Basak
+ * @creation_date 27th Sept 2019 11.48am
+ * @modification_date 25th sept 2019 11.48am
+ * @version 2.0
+ * @copyright Zensar Technologies.All rights reserved
+ * @description It is a persistent Class.
+ *  It is a WageEmp Bean Class.
+ *
+ */
+@Entity
+public class WageEmp extends Employee {
+	private int hours;
+	private float rate;
+	
+	
+	public WageEmp() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public WageEmp(int empId, String name, LocalDate joinDate, double salary, int hours, float rate) {
+		super(empId, name, joinDate, salary);
+		this.hours = hours;
+		this.rate = rate;
+	}
+
+	
+
+	public int getHours() {
+		return hours;
+	}
+
+
+	public void setHours(int hours) {
+		this.hours = hours;
+	}
+
+
+	public float getRate() {
+		return rate;
+	}
+
+
+	public void setRate(float rate) {
+		this.rate = rate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "WageEmp [hours=" + hours + ", rate=" + rate + "]";
+	}
+	
+	
+	
+
+}
